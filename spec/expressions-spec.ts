32d424c9ee39c04b3ecb038d7476a1a98f6b89c2
Thomas Hickman <Thomas.Hickman42@gmail.com>
2016-12-23 11:24:42 +0000

    Initial expression parsing Also added test

---
+++ spec/expressions-spec.ts
@@ -0,0 +1,26 @@
+/// <reference path="../node_modules/@types/jasmine/index.d.ts" />
+import { parseExpression } from '../src/expressions';
+import * as expr from "../src/expressions";
+
+var testLiterals:Literal[] = [{
+    type: "Selector",
+    text: "#input"
+},{
+    type: "Id",
+    text: "shows"
+},{
+    type: "String",
+    text: "hi",
+    code: "'hi'"
+}]
+
+describe("expressions", () => {
+    it("parses an expression", () => {
+        var expression = parseExpression(testLiterals);
+        expect(expression).toEqual({
+            left: testLiterals[0],
+            operator: testLiterals[1],
+            right: testLiterals[2]
+        })
+    })
+})
--- src/expressions.ts
+++ src/expressions.ts
@@ -1,24 +1,106 @@
 import { error } from './common';
 import * as _ from 'lodash';
 
-export interface Expression{
-    func: (left: any, right: any) => any, // define using arrow functions
-    valid?: (left: Expression, right: Expression) => boolean,
+export interface ExpressionFunc{
+    type: [Type, Type],
+    func: (left: any, right: any) => any
+}
+
+export interface Operator{
+    funcs: ExpressionFunc[],
+    valid?: (left: Operator, right: Operator) => boolean,
     name: string,
     priority: number,
     leftAccoc: boolean
 }
 
-var expressions = <Expression[]>[{
-        name: "says",
+type Type = "String" | "Selector" | "Any";
+
+var operators: Operator[] = [{
+        name: "shows",
         priority: 1,
         leftAccoc: true,
-        func: (left: any, right: any) => {
-            return left.val() === right
-        }
+        funcs: [{
+            type: ["Selector", "Any"],
+            func: (left: any, right: any) => left.val() === right
+        }]
     }
 ]
 
+var operatorTable = _.mapValues(_.groupBy(operators, "name"), arr => {
+    if(arr.length !== 1){
+        throw error("Internal Error: Operator names are not disjoint")
+    }
+    return arr[0]
+});
+
+var expressionsByPriority = _.sortBy(operators, "priority");
+
 var funcParse = /return (.*?)(?:\n|;)/;
 
-export default expressions;
+function getOperator(literal: Literal): Operator | undefined{
+    if(literal.type === "Id" || literal.type === "Symbol"){
+        return operatorTable[literal.text]
+    }
+
+    return;
+}
+
+export type Expression = Literal | {
+    left: Expression,
+    operator: Operator,
+    right: Expression
+}
+
+export function parseExpression(literals: Literal[]){
+    debugger;
+    var output = <Expression[]>[];
+    var operatorStack = <Operator[]>[];
+
+    function consumeOperator(){
+        var [left, right] = output.splice(-2);
+        var lastOperator = operatorStack.pop();
+        if(lastOperator === undefined){
+            throw error("Operator stack is empty!");
+        }
+
+        output.push({
+            left: left,
+            operator: lastOperator,
+            right: right
+        })
+    }
+
+    for(var literal of literals){
+        var currOperator = getOperator(literal);
+
+        if(currOperator === undefined){
+            output.push(literal);
+        }
+        else{
+            var topOperator = _.last(operatorStack);
+
+            while(operatorStack.length !== 0 &&
+                ((topOperator.leftAccoc && topOperator.priority <= currOperator.priority)
+                ||(!topOperator.leftAccoc && topOperator.priority <= currOperator.priority))
+            ){
+                // consume operator
+                consumeOperator();
+
+                topOperator = _.last(operatorStack);
+            }
+        }
+    }
+
+    while(operatorStack.length !== 0){
+        consumeOperator();
+    }
+
+    if(output.length !== 1){
+        throw error("Output is not 1");
+    }
+
+    return output[0];
+}
+
+export default operators;
