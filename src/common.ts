5dcfa26fe4f2f7d521ad5f8d4c055cf3ec272a18
Thomas Hickman <Thomas.Hickman42@gmail.com>
2016-12-28 19:07:04 +0000

    Added location to error

--- src/common.ts
+++ src/common.ts
@@ -1,6 +1,10 @@
 import * as _ from 'lodash';
 
-export function error(message: string){
+export function error(message: string, location?: Location){
+    if(location !== undefined){
+        message += `\nLocation: ${JSON.stringify(location)}`
+    }
+
     return {
         ssGenerated: true,
         message: message,
